6221364b7e3c0bea9240f54f3ea5f9d83f98f9e2
LegendRK <rgk8966@g.rit.edu>
2018-05-27 17:56:32 -0700

    Added at least one thing to each folder, starting making the timer

---
+++ Where's Alaska 2/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+    [SerializeField]
+    private float time;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
